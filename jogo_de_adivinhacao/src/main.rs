b7f9ff95345c0c3a1f7619bdefdeca84aadb2236
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-10-10 14:21:09 -0300

    feat: generate random number with `rand` lib

--- jogo_de_adivinhacao/src/main.rs
+++ jogo_de_adivinhacao/src/main.rs
@@ -1,7 +1,14 @@
+extern crate rand;
+
+use rand::Rng;
 use std::io;
 
 fn main() {
     println!("Adivinhe o número!");
+
+    let numero_secreto = rand::thread_rng().gen_range(1, 11);
+
+    println!("O número secreto é {numero_secreto}");
 
     println!("Digite seu palpite: ");
 
