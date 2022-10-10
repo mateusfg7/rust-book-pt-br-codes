dbdf1415d42edc1091b5fc3357e11370ee0eb86c
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-10-10 12:13:01 -0300

    feat: add initial code
    * Shows messages
    * Get user input

--- jogo_de_adivinhacao/src/main.rs
+++ jogo_de_adivinhacao/src/main.rs
@@ -1,3 +1,15 @@
+use std::io;
+
 fn main() {
-    println!("Hello, world!");
+    println!("Adivinhe o número!");
+
+    println!("Digite seu palpite: ");
+
+    let mut palpite = String::new();
+
+    io::stdin()
+        .read_line(&mut palpite)
+        .expect("Falha ao let entrada");
+
+    print!("Você disse: {palpite}")
 }
