b132123041ae45ef2b2a039a5a6e7efd8d2d3682
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-10-10 14:48:18 -0300

    feat: compare numbers

--- jogo_de_adivinhacao/src/main.rs
+++ jogo_de_adivinhacao/src/main.rs
@@ -1,6 +1,7 @@
 extern crate rand;
 
 use rand::Rng;
+use std::cmp::Ordering;
 use std::io;
 
 fn main() {
@@ -18,5 +19,18 @@ fn main() {
         .read_line(&mut palpite)
         .expect("Falha ao let entrada");
 
-    print!("Você disse: {palpite}")
+    /// `trim()` will remove any whitespace before and after, or \n (newline), entered by enter key.
+    /// `parse()` will convert a string to a variety of number types
+    let palpite: u32 = palpite
+        .trim()
+        .parse()
+        .expect("Por favor, digite um número!");
+
+    println!("Você disse: {palpite}");
+
+    match palpite.cmp(&numero_secreto) {
+        Ordering::Less => println!("Muito baixo!"),
+        Ordering::Greater => println!("Muito alto!"),
+        Ordering::Equal => println!("Você acertou!"),
+    }
 }
