265274c623463676e366ac6a333b8b4199568d81
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-10-11 11:22:45 -0300

    add functions

---
+++ functions/src/main.rs
@@ -0,0 +1,26 @@
+// fn cinco() -> i32 {
+//     5
+// }
+
+// fn soma_um(x: i32) -> i32 {
+//     x + 1
+//     // x + 1; /* error */
+// }
+
+fn main() {
+    // let x = 5;
+    //
+    // Cria um novo escopo
+    // let y = {
+    //     let x = 3;
+    //     x + 1 // expressões não possuem `;`, senão se tornam declarações, e não retornam valor
+    // };
+    //
+    // println!("O valor de y é {y}")
+
+    // let x = cinco();
+    // println!("O valor de x é {x}")
+
+    // let x = soma_um(5);
+    // println!("O valor de x é {x}")
+}
