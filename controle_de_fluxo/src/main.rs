caf7b7290a9a32bc5f420031733bda45198564d4
Mateus Felipe Gonçalves <mateusfelipefg77@gmail.com>
2022-10-11 13:02:38 -0300

    add controle_de_fluxo

---
+++ controle_de_fluxo/src/main.rs
@@ -0,0 +1,45 @@
+fn main() {
+    // let numero = 3;
+    //
+    // if numero < 5 {
+    //     println!("condição era verdadeira!");
+    // } else {
+    //     println!("condição era falsa");
+    // }
+
+    // let condicao = true;
+    // let numero = if condicao { 5 } else { 6 };
+    // println!("O valor de numero é {numero}");
+
+    // let numero = if condicao { 5 } else { "seis" }; /* error: tipos incompatíveis */
+    //
+
+    /* LAÇOS */
+    // Loop
+    // loop {
+    //     println!("Novamente!");
+    // }
+
+    // While
+    // let mut numero = 3;
+    //
+    // while numero != 0 {
+    //     println!("{numero}!");
+    //     numero -= 1;
+    // }
+    //
+    // println!("LIFTOFF!!!");
+
+    // For
+    // let a = [10, 20, 30, 40, 50];
+    //
+    // for elemento in a.iter() {
+    //     println!("O valor é: {elemento}")
+    // }
+    //
+    // Contagem regressiva
+    //
+    // for numero in (1..4).rev() {
+    //     println!("{numero}")
+    // }
+}
